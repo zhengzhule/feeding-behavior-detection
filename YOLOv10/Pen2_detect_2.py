eec1763c5a7e645dec0c62fa04b4d17a2102e0a0
zhengzhule <133102825+zhengzhule@users.noreply.github.com>
2024-08-31 17:59:42 +0800

    code

---
+++ YOLOv10/Pen2_detect_2.py
@@ -0,0 +1,275 @@
+import cv2
+import torch
+from ultralytics import YOLO
+import os
+import argparse
+from strong_sort.strong_sort import StrongSORT
+from collections import deque
+import numpy as np
+from pathlib import Path
+total_processed_frames = {"三分之一帧数":242584}
+import glob
+def xyxy2xywh(x):
+    # Convert nx4 boxes from [x1, y1, x2, y2] to [x, y, w, h] where xy1=top-left, xy2=bottom-right
+    y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
+    y[:, 0] = (x[:, 0] + x[:, 2]) / 2  # x center
+    y[:, 1] = (x[:, 1] + x[:, 3]) / 2  # y center
+    y[:, 2] = x[:, 2] - x[:, 0]  # width
+    y[:, 3] = x[:, 3] - x[:, 1]  # height
+    return y
+ROOT = Path(__file__).resolve().parent
+parser = argparse.ArgumentParser()
+# 检测参数
+parser.add_argument('--show-vid', action='store_true', default=False, help='display tracking video results')
+parser.add_argument('--weights', default=r"D:\采食文章部\improvedyolov10-strongsort\runs\trainFINALV10\exp\weights\best.pt", type=str, help='weights path')
+parser.add_argument('--source', default=r'D:\采食文章部\yolov8_trackingdabao-SEAM\6pmnew', type=str, help='video(.mp4)path')
+parser.add_argument('--project', default=ROOT / 'runs' / 'track1gpu6pm', help='save results to project/name')
+parser.add_argument('--name', default='exp', help='save results to project/name')
+parser.add_argument('--vis', default=True, action='store_true', help='visualize image')
+parser.add_argument('--conf_thre', type=float, default=0.5, help='conf_thre')
+parser.add_argument('--iou_thre', type=float, default=0.5, help='iou_thre')
+parser.add_argument('--vid-stride', type=int, default=3, help='video frame-rate stride')
+parser.add_argument('--line-thickness', default=2, type=int, help='bounding box thickness (pixels)')
+parser.add_argument('--save-vid', action='store_true', default=False, help='save video tracking results')
+# 跟踪参数
+parser.add_argument('--track_model', default=r"./track_models/osnet_x0_25_msmt17.pt", type=str, help='track model')
+parser.add_argument('--max_dist', type=float, default=0.2, help='max dist')
+parser.add_argument('--max_iou_distance', type=float, default=0.7, help='max_iou_distance')
+parser.add_argument('--max_age', type=int, default=70, help='max_age')
+parser.add_argument('--n_init', type=int, default=3, help='n_init')
+parser.add_argument('--nn_budget', type=int, default=100, help='nn_budget')
+# 解析参数
+opt = parser.parse_args()
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+class Detector(object):
+    def __init__(self, weight_path, conf_threshold=0.2, iou_threshold=0.5, line_thickness=2):
+        self.device = device
+        self.model = YOLO(weight_path).to(self.device)
+        self.conf_threshold = conf_threshold
+        self.iou_threshold = iou_threshold
+        self.line_thickness = line_thickness  # Save the line_thickness parameter
+
+        self.tracker = StrongSORT(
+            opt.track_model,
+            self.device,
+            max_dist=opt.max_dist,
+            max_iou_distance=opt.max_iou_distance,
+            max_age=opt.max_age,
+            n_init=opt.n_init,
+            nn_budget=opt.nn_budget,
+        )
+
+        self.trajectories = {}
+        self.max_trajectory_length = 5
+
+    @staticmethod
+    def get_color(idx):
+        idx = idx * 3
+        color = ((37 * idx) % 255, (17 * idx) % 255, (29 * idx) % 255)
+        return color
+
+    def write_results(self, frame_idx, txt_path, online_targets):
+        with open(txt_path, 'a') as f:
+            for t in online_targets:
+                bbox = t[:4]
+                tid = t[4]
+                cls = t[5]
+                conf = t[6]
+                bbox_left, bbox_top, bbox_right, bbox_bottom = bbox
+                bbox_w = bbox_right - bbox_left
+                bbox_h = bbox_bottom - bbox_top
+                x1 = (bbox_left + bbox_right) / 2
+                y1 = bbox_top
+                # 4号圈早
+                # if x1 < 1700 and y1 > 222:
+                #     h = 1.97605748e-10 * x1 ** 4 - 7.59015002e-07 * x1 ** 3 + 9.05807475e-04 * x1 ** 2 - 3.37221616e-01*x1 ** 1 + 4.79487679e+02
+                    # 4号圈晚
+                    # if 370 < x1 < 2100 and y1 > 1000:
+                    #     h =  5.70176957e-08 * x1 ** 3 -2.52976561e-04 * x1 ** 2  +3.40239992e-01 * x1 + 1.07687571e+03
+
+                    # 6号圈早
+                    #    if  < 2142 and y1 > 1015:
+                    #     h =  6.43097509e-08  * x1 ** 3 -2.69096291e-04 * x1 ** 2 +3.34666588e-01  * x1 + 7.69840281e+02
+
+                    # 6号圈晚
+
+                if x1 < 1760 and y1 > 209:
+                    h = 2.50828543e-10 * x1 ** 4 - 9.64794675e-07 * x1 ** 3 + 1.23552971e-03 * x1 ** 2 - 6.27365541e-01 * x1 ** 1 + 5.42271623e+02
+
+                    # 格式: frame_idx, id, bbox_left, bbox_top, bbox_w, bbox_h, y1, h, conf, cls, -1
+                    f.write(
+                        f"{total_processed_frames['三分之一帧数']} {frame_idx + 1} {int(tid)} {bbox_left:.2f} {bbox_top:.2f} {bbox_w:.2f} {bbox_h:.2f} {y1:.2f} {h:.2f} {conf:.2f} {int(cls)} -1\n")
+
+    def detect_image(self, img_bgr, frame_idx, txt_path):
+        image = img_bgr.copy()
+        results = self.model(img_bgr, conf=self.conf_threshold, iou=self.iou_threshold)
+        boxes = results[0].boxes.xyxy.cpu().numpy()  # xyxy format
+        confidences = results[0].boxes.conf
+        class_preds = results[0].boxes.cls
+
+        confidences_expanded = confidences.unsqueeze(1)
+        class_preds_expanded = class_preds.unsqueeze(1)
+        boxes_tensor = torch.from_numpy(boxes).to(class_preds_expanded.device)
+        xywhs = xyxy2xywh(boxes_tensor)
+        online_targets = self.tracker.update(xywhs.cpu(), confidences_expanded.cpu(), class_preds_expanded.cpu(), image)
+
+        # 将结果写入文件
+        self.write_results(frame_idx, txt_path, online_targets)
+
+        for t in online_targets:
+            tlwh = [t[0], t[1], t[2] - t[0], t[3] - t[1]]
+            tid = t[4]
+            cls = t[5]
+            xmin, ymin, xmax, ymax = tlwh[0], tlwh[1], tlwh[0] + tlwh[2], tlwh[1] + tlwh[3]
+            class_pred = int(cls)
+            color = self.get_color(class_pred + 4)
+            center = (int(xmin + xmax) // 2, int(ymin + ymax) // 2)
+            bbox_label = results[0].names[class_pred]
+
+            x1 = (xmin + xmax) / 2
+            y1 = ymin
+
+            # 4号圈早
+            # if x1 < 1700 and y1 > 222:
+            #     h = 1.97605748e-10 * x1 ** 4 - 7.59015002e-07 * x1 ** 3 + 9.05807475e-04 * x1 ** 2 - 3.37221616e-01 * x1 ** 1 + 4.79487679e+02
+
+                # 4号圈晚
+                # if 370 < x1 < 2100 and y1 > 1000:
+                #     h =  5.70176957e-08 * x1 ** 3 -2.52976561e-04 * x1 ** 2  +3.40239992e-01 * x1 + 1.07687571e+03
+
+                # 6号圈早
+                #    if  < 2142 and y1 > 1015:
+                #     h =  6.43097509e-08  * x1 ** 3 -2.69096291e-04 * x1 ** 2 +3.34666588e-01  * x1 + 7.69840281e+02
+
+                # 6号圈晚
+
+            if x1 < 1760 and y1 > 209:
+                h = 2.50828543e-10 * x1 ** 4 - 9.64794675e-07 * x1 ** 3 + 1.23552971e-03 * x1 ** 2 - 6.27365541e-01 * x1 ** 1 + 5.42271623e+02
+
+                if ymax > h:
+                    act = 'Feeding'
+                elif ymax < h:
+                    act = 'Picking'
+                else:
+                    act = 'ing'
+
+                bbox_label = f'{bbox_label} {int(tid)} {act}'
+
+            cv2.rectangle(img_bgr, (int(xmin), int(ymin)), (int(xmax), int(ymax)), color, self.line_thickness)
+
+
+            if tid not in self.trajectories:
+                self.trajectories[tid] = deque(maxlen=self.max_trajectory_length)
+            self.trajectories[tid].appendleft(center)
+
+            # 截断轨迹长度
+            if len(self.trajectories[tid]) > self.max_trajectory_length:
+                self.trajectories[tid] = self.trajectories[tid][:self.max_trajectory_length]
+
+            for i in range(1, len(self.trajectories[tid])):
+                if self.trajectories[tid][i - 1] is None or self.trajectories[tid][i] is None:
+                    continue
+
+                thickness = int(np.sqrt(64 / float(i + 1)))
+                cv2.line(img_bgr, self.trajectories[tid][i - 1],
+                         self.trajectories[tid][i], color,
+                         thickness)
+
+            # 显示类名和跟踪ID
+            cv2.putText(img_bgr, bbox_label,
+                        (int(xmin), int(ymin - 5)), cv2.FONT_HERSHEY_COMPLEX, 0.6, color, 2)
+
+        return img_bgr
+
+
+# Example usage
+if __name__ == '__main__':
+    model = Detector(weight_path=opt.weights, conf_threshold=opt.conf_thre, iou_threshold=opt.iou_thre,
+                     line_thickness=opt.line_thickness)
+
+
+def process_video(video_file, opt):
+    opt.source = video_file
+    # Initialize the Detector with the line thickness argument
+    model = Detector(weight_path=opt.weights, conf_threshold=opt.conf_thre, iou_threshold=opt.iou_thre,
+                     line_thickness=opt.line_thickness)
+
+    # 创建新的项目文件夹
+    save_dir = Path(opt.project) / opt.name
+    save_dir.mkdir(parents=True, exist_ok=True)
+
+    # 寻找下一个可用的exp文件夹
+    exp_number = 1
+    while (save_dir / f'exp{exp_number}').exists():
+        exp_number += 1
+    save_dir = save_dir / f'exp{exp_number}'
+    save_dir.mkdir(parents=True, exist_ok=True)
+
+    capture = cv2.VideoCapture(opt.source)
+    fps = capture.get(cv2.CAP_PROP_FPS)
+    size = (int(capture.get(cv2.CAP_PROP_FRAME_WIDTH)),
+            int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+
+    # 创建输出视频文件路径
+    video_path = save_dir / f"{Path(opt.source).stem}_out.mp4"
+    outVideo = cv2.VideoWriter(str(video_path), fourcc, fps, size)
+
+    # 创建txt文件路径
+    txt_path = save_dir / f"{Path(opt.source).stem}_results.txt"
+
+    # 创建或清空txt文件
+    open(txt_path, 'w').close()
+
+    frame_idx = 0
+    while True:
+        ret, frame = capture.read()
+        if not ret:
+            break
+        # 使用 vid-stride 参数
+        if frame_idx % opt.vid_stride == 0:
+            img_vis = model.detect_image(frame, frame_idx, txt_path)
+            outVideo.write(img_vis)
+            # 在这里增加计数器
+            total_processed_frames["三分之一帧数"] += 1
+
+            # Check if --show-vid is set
+            if opt.show_vid:
+                # 创建可调整大小的窗口
+                cv2.namedWindow('track', cv2.WINDOW_NORMAL)
+
+                # 重新调整显示图像的大小
+                img_vis = cv2.resize(img_vis, None, fx=1, fy=1, interpolation=cv2.INTER_NEAREST)
+
+                # 显示图像
+                cv2.imshow('track', img_vis)
+
+                # 你可以使用 `cv2.resizeWindow` 来手动设置窗口大小
+                cv2.resizeWindow('track', 1800, 580)  # 你可以将800x600替换为任何你想要的窗口大小
+
+                # 等待用户按键（这里的 30 表示 30ms 后继续下一帧，按 'q' 退出）
+                if cv2.waitKey(30) & 0xFF == ord('q'):
+                    break
+        frame_idx += 1
+
+    capture.release()
+    outVideo.release()
+
+    # Destroy windows only if --show-vid is set
+    if opt.show_vid:
+        cv2.destroyAllWindows()
+
+    print(f"Results saved to {save_dir}")
+
+def main(opt):
+    # 查找文件夹中的所有视频文件
+    video_files = sorted(glob.glob(os.path.join(opt.source, '*.mp4')))
+
+    # 处理每个视频文件
+    for video_file in video_files:
+        process_video(video_file, opt)
+
+if __name__ == '__main__':
+    opt = parser.parse_args()
+    main(opt)
+
---
+++ YOLOv10/ultralytics/nn/Addmodules/YOLOFace.py
@@ -0,0 +1,188 @@
+import torch
+import torch.nn as nn
+
+__all__ = ['SEAM', 'MultiSEAM', 'PSASEAM', 'PSAMultiSEAM']
+
+class Residual(nn.Module):
+    def __init__(self, fn):
+        super(Residual, self).__init__()
+        self.fn = fn
+
+    def forward(self, x):
+        return self.fn(x) + x
+
+class SEAM(nn.Module):
+    def __init__(self, c1,  n=1, reduction=16):
+        super(SEAM, self).__init__()
+        c2 = c1
+        self.DCovN = nn.Sequential(
+            # nn.Conv2d(c1, c2, kernel_size=3, stride=1, padding=1, groups=c1),
+            # nn.GELU(),
+            # nn.BatchNorm2d(c2),
+            *[nn.Sequential(
+                Residual(nn.Sequential(
+                    nn.Conv2d(in_channels=c2, out_channels=c2, kernel_size=3, stride=1, padding=1, groups=c2),
+                    nn.GELU(),
+                    nn.BatchNorm2d(c2)
+                )),
+                nn.Conv2d(in_channels=c2, out_channels=c2, kernel_size=1, stride=1, padding=0, groups=1),
+                nn.GELU(),
+                nn.BatchNorm2d(c2)
+            ) for i in range(n)]
+        )
+        self.avg_pool = torch.nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(c2, c2 // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(c2 // reduction, c2, bias=False),
+            nn.Sigmoid()
+        )
+
+        self._initialize_weights()
+        # self.initialize_layer(self.avg_pool)
+        self.initialize_layer(self.fc)
+
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.DCovN(x)
+        y = self.avg_pool(y).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        y = torch.exp(y)
+        return x * y.expand_as(x)
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.xavier_uniform_(m.weight, gain=1)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def initialize_layer(self, layer):
+        if isinstance(layer, (nn.Conv2d, nn.Linear)):
+            torch.nn.init.normal_(layer.weight, mean=0., std=0.001)
+            if layer.bias is not None:
+                torch.nn.init.constant_(layer.bias, 0)
+
+
+def DcovN(c1, c2, depth, kernel_size=3, patch_size=3):
+    dcovn = nn.Sequential(
+        nn.Conv2d(c1, c2, kernel_size=patch_size, stride=patch_size),
+        nn.SiLU(),
+        nn.BatchNorm2d(c2),
+        *[nn.Sequential(
+            Residual(nn.Sequential(
+                nn.Conv2d(in_channels=c2, out_channels=c2, kernel_size=kernel_size, stride=1, padding=1, groups=c2),
+                nn.SiLU(),
+                nn.BatchNorm2d(c2)
+            )),
+            nn.Conv2d(in_channels=c2, out_channels=c2, kernel_size=1, stride=1, padding=0, groups=1),
+            nn.SiLU(),
+            nn.BatchNorm2d(c2)
+        ) for i in range(depth)]
+    )
+    return dcovn
+
+class MultiSEAM(nn.Module):
+    def __init__(self, c1,  depth=1, kernel_size=3, patch_size=[3, 5, 7], reduction=16):
+        super(MultiSEAM, self).__init__()
+        c2 = c1
+        self.DCovN0 = DcovN(c1, c2, depth, kernel_size=kernel_size, patch_size=patch_size[0])
+        self.DCovN1 = DcovN(c1, c2, depth, kernel_size=kernel_size, patch_size=patch_size[0])
+        self.DCovN2 = DcovN(c1, c2, depth, kernel_size=kernel_size, patch_size=patch_size[0])
+        self.avg_pool = torch.nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(c2, c2 // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(c2 // reduction, c2, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y0 = self.DCovN0(x)
+        y1 = self.DCovN1(x)
+        y2 = self.DCovN2(x)
+        y0 = self.avg_pool(y0).view(b, c)
+        y1 = self.avg_pool(y1).view(b, c)
+        y2 = self.avg_pool(y2).view(b, c)
+        y4 = self.avg_pool(x).view(b, c)
+        y = (y0 + y1 + y2 + y4) / 4
+        y = self.fc(y).view(b, c, 1, 1)
+        y = torch.exp(y)
+        return x * y.expand_as(x)
+
+
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    """Pad to 'same' shape outputs."""
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
+
+class Conv(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+
+    default_act = nn.SiLU()  # default activation
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
+    def forward(self, x):
+        """Apply convolution, batch normalization and activation to input tensor."""
+        return self.act(self.bn(self.conv(x)))
+
+    def forward_fuse(self, x):
+        """Perform transposed convolution of 2D data."""
+        return self.act(self.conv(x))
+
+class PSASEAM(nn.Module):
+
+    def __init__(self, c1, c2, e=0.5):
+        super().__init__()
+        assert (c1 == c2)
+        self.c = int(c1 * e)
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv(2 * self.c, c1, 1)
+
+        self.attn = SEAM(self.c)
+        self.ffn = nn.Sequential(
+            Conv(self.c, self.c * 2, 1),
+            Conv(self.c * 2, self.c, 1, act=False)
+        )
+
+    def forward(self, x):
+        a, b = self.cv1(x).split((self.c, self.c), dim=1)
+        b = b + self.attn(b)
+        b = b + self.ffn(b)
+        return self.cv2(torch.cat((a, b), 1))
+
+
+
+class PSAMultiSEAM(nn.Module):
+
+    def __init__(self, c1, c2, e=0.5):
+        super().__init__()
+        assert (c1 == c2)
+        self.c = int(c1 * e)
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv(2 * self.c, c1, 1)
+
+        self.attn = MultiSEAM(self.c)
+        self.ffn = nn.Sequential(
+            Conv(self.c, self.c * 2, 1),
+            Conv(self.c * 2, self.c, 1, act=False)
+        )
+
+    def forward(self, x):
+        a, b = self.cv1(x).split((self.c, self.c), dim=1)
+        b = b + self.attn(b)
+        b = b + self.ffn(b)
+        return self.cv2(torch.cat((a, b), 1))
